af2d853b8ccebbe4563b138eb942cafe164ff8ea
PaulChass <paulchasseuil@hotmail.fr>
2024-06-28 13:16:39 +0200

    css rule update

--- src/components/DownloadFolder.js
+++ src/components/DownloadFolder.js
@@ -29,7 +29,7 @@ const DownloadFolder = ({ folderId, noText }) => {
     };
 
     return (<div>
-<button onClick={handleDownload} disabled={isLoading}>
+<button className='bigButton' onClick={handleDownload} disabled={isLoading}>
           {isLoading ? 'Downloading...' : 'Download This Folder'}
         </button>{isLoading && <h3>Be patient... this could take a while 😉</h3>}</div>    );
 };
